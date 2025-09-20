ff1ae5d79ddf6eabe98321ba4c5d734c20900df7
komal vaishnav <komalvaishnav2812@gmail.com>
2025-09-20 10:32:57 +0530

    add home page sections and register user page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Footer from './components/layout/Footer.jsx'
 import Navbar from './components/layout/Navbar.jsx'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login'
+import Register from './pages/Register.jsx'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/courses" element={<h1 className="text-center mt-5">Courses Page</h1>} />
       </Routes>
       <Footer />
