71cb745dcc486d149ad29d38b4e2a43acd9d2700
komal vaishnav <komalvaishnav2812@gmail.com>
2025-09-20 11:22:29 +0530

    updated deeder file

--- backened/data/seeder.js
+++ backened/data/seeder.js
@@ -1,73 +1,85 @@
 import fs from "fs";
-import Course from "../models/CourseModel.js";
-import User from "../models/UserModel.js";
 import Category from "../models/CategoryModel.js";
-import path from "path";
-import bcrypt from "bcrypt";
+import Course from "../models/CourseModel.js"
+import User from "../models/UserModel.js";
+import path from 'path';
+import dotenv from "dotenv";
+import bcrypt from 'bcrypt';
+dotenv.config();
+import connectDB from '../config/db.js';
+// connect to db
+connectDB();
+const __dirname = path.resolve();
+
 
 const importData = async () => {
-  try {
-    //  clear the database
+    try {
+        console.log('Clearing database!');
+        // clean the database
+        await User.deleteMany();
+        await Course.deleteMany();
+        await Category.deleteMany();
+
+        // insert the data
+        console.log("__dirname", __dirname);
+
+        // insert user data
+        const usersData = JSON.parse(fs.readFileSync(path.join(__dirname, '/data/users.json'), 'utf-8'));
+
+        const usersWithHashedPassword = usersData.map((user) => {
+            // create password hash
+            const salt = bcrypt.genSaltSync(10);
+            const hashedPassword = bcrypt.hashSync(user.password, salt);
+            return {...user, password: hashedPassword};
+        });
+
+        const createdUsers = await User.insertMany(usersWithHashedPassword);
+
+        const instructorUser = createdUsers.find((user) => user.role === 'instructor')  // to be used in course as ref
+        
+
+        // insert category data
+        const categoryData = JSON.parse(fs.readFileSync(path.join(__dirname, '/data/category.json'), 'utf-8'));
+        const createdCategory = await Category.insertMany(categoryData);
+
+        const webdevCategory = createdCategory.find((category) => category.name === 'Web Dev')// to be used in course as ref
+
+        // insert course
+        const courses = [{
+            title: 'Complete Web dev course 2025',
+            description: 'Random text about the course: command not found: asdfsdf',
+            price: 99,
+            instructor: instructorUser.id,
+            category: webdevCategory.id
+        }]
+
+        await Course.insertMany(courses);
+
+        console.log('Data is successfully added to database!');
+        process.exit();
+    } catch (error) {
+        console.log("Error while adding data: ", error);
+        process.exit(1);
+    }
+
+}
+// remove data
+const destroyData = async () => {
+    // deleteMany queries will go here!
+    // clean the database
+    await User.deleteMany();
     await Course.deleteMany();
     await Category.deleteMany();
-    await User.deleteMany();
-    // import the data
-    const usersData = JSON.parse(
-      fs.readFileSync(path.join(__dirname, "/data/users.json"), "utf-8")
-    );
-    const usersWithHashedPass = usersData.map((user) => {
-      const salt = bcrypt.genSaltSync(10);
-      const hashedpassword = bcrypt.hashSync(user.password, salt);
-      return { ...user, password: hashedpassword };
-    });
-
-    const createdUsers = await User.insertMany(usersWithHashedPass);
-    const instructorUser = createdUsers.find(
-      (user) => user.role === "instructor"
-    );
-
-    // insert category data
-    const categoryData = JSON.parse(
-      fs.readFileSync(path.join(__dirname, "/data/category.json"), "utf-8")
-    );
-    const createdCategory = await Category.insertMany(categoryData);
-
-    const webdevCategory = createdCategory.find(
-      (category) => category.name === "Web Dev"
-    );
-    // insert course data
-    const courses = [
-      {
-        title: "Complete Web Dev Course 2025",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        price: 199.99,
-        category: webdevCategory.id,
-        instructor: instructorUser.id,
-      },
-    ];
-
-    await Course.insertMany(courses);
-    console.log("Data imported successfully");
+    console.log("Data destroyed")
     process.exit();
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
 
-  // remove data
-  const destroyData = () => {
-    // deleteMany queries will go here!
-    console.log("Data destroyed successfully");
-    process.exit(); 
-  };
+}
 
-  // logic to add script to run seed file for different methods
-  // logic to add script to run seed file for different methods
-  if (process.argv[2] === "-d") {
+// logic to add script to run seed file for different methods
+if (process.argv[2] === '-d') {
+
     destroyData();
-  } else {
-    importData("import data");
+} else {
+    importData();
     console.log("import data");
-  }
-};
+}
